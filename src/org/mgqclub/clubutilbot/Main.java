cf96ee521b057b5e47bd5f2613b41df12e6be1fd
megukkyu <megukkyu@excite.co.jp>
2018-02-20 01:40:47 +0900

    メインクラス制作

--- src/org/mgqclub/clubutilbot/Main.java
+++ src/org/mgqclub/clubutilbot/Main.java
@@ -1,10 +1,19 @@
 package org.mgqclub.clubutilbot;
 
+import sx.blah.discord.api.IDiscordClient;
+
 public class Main {
 
+	private static IDiscordClient client;
+	private static String loginToken;
+	s
+	
 	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("引数が間違っています");
+			System.exit(1);
+		}
 		
-
 	}
 
 }
