8dcece205205e8b9f4578a25907f2bff65b0d42a
megukkyu <megukkyu@excite.co.jp>
2018-02-20 10:02:44 +0900

    fix conflict

--- src/org/mgqclub/clubutilbot/Main.java
+++ src/org/mgqclub/clubutilbot/Main.java
@@ -1,10 +1,34 @@
 package org.mgqclub.clubutilbot;
 
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.util.DiscordException;
+
 public class Main {
 
+	private static IDiscordClient client;
+	private static String loginToken;
+	
+	
 	public static void main(String[] args) {
+		System.out.println("起動開始");
+		if (args.length < 2) {
+			System.out.println("引数が間違っています");
+			System.exit(1);
+		}
 		
-
+		
 	}
+	
+    public static IDiscordClient createClient(String token) { // Returns a new instance of the Discord client
+        ClientBuilder clientBuilder = new ClientBuilder(); // Creates the ClientBuilder instance
+        clientBuilder.withToken(token); // Adds the login info to the builder
+        try {
+        	return clientBuilder.login(); // Creates the client instance and logs the client in
+        } catch (DiscordException e) { // This is thrown if there was a problem building the client
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
